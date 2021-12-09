79a245c29fac7adcc674eb3cac5dc3d88514be26
Josef <josefpopov1990@gmail.com>
2021-12-09 16:36:59 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// for (let i = 0; i < 300; i ++) {
+//     console.log(  i);
+
+// }
+
+let value = Math;
+
+value = Math.floor(Math.random() * 10 + 1)
+
+document.write(value)
+
+var nj = [{
+    nickname: "josef",
+    subname: "popov"
+}]
+
+console.table(nj)
+
+console.log(value)
+
+//let  anweser=confirm("hi");
+
+//console.log(anweser)
